37e811cf459c6cad255b55ec2edfe5695ef9e7e3
Valentina-Fr <v_frasca@virgilio.it>
2021-07-29 14:17:36 +0200

    Install Vue and set layout

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log("test vue". Vue);
+Vue.config.devtools = true;
+
+const app = new Vue ({
+    el: "#app",
+    data: {
+        toDoList: ["Fare la spesa", "Fare il bucato", "Pagare bollette", "Pulire casa"],
+    }, 
+    methods: {
+
+    }
+});
